705cb044d4960b4080fc4512ab1b03bd20e53820
Jeremenkovic <102044657+Jeremenkovic@users.noreply.github.com>
2022-11-27 04:05:39 +0100

    update

--- lib/countdown_timer.dart
+++ lib/countdown_timer.dart
@@ -10,18 +10,15 @@ class CountDownTimer extends StatefulWidget {
   _CountDownTimerState createState() => _CountDownTimerState();
 }
 
-class _CountDownTimerState extends State<CountDownTimer> 
+class _CountDownTimerState extends State<CountDownTimer>
     with TickerProviderStateMixin {
   late AnimationController controller;
   late AnimationController controller1;
   late AnimationController controller2;
   late AnimationController controller3;
   late AnimationController lottiecontroller;
-  late AnimationController lottiecontroller1;
-  late AnimationController lottiecontroller2;
-  late AnimationController lottiecontroller3;
+
   int cont = 0;
-  
 
   List<Duration> animationDuration = [
     const Duration(seconds: 5),
@@ -31,8 +28,8 @@ class _CountDownTimerState extends State<CountDownTimer>
   ];
 
   String get timerString {
-    Duration duration =
-        controller.duration! * controller.value + const Duration(milliseconds: 999);
+    Duration duration = controller.duration! * controller.value +
+        const Duration(milliseconds: 999);
     return '${duration.inSeconds}';
   }
 
@@ -65,26 +62,13 @@ class _CountDownTimerState extends State<CountDownTimer>
       vsync: this,
       duration: const Duration(seconds: 5),
     );
-    lottiecontroller1 = AnimationController(
+
+    lottiecontroller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 5),
+      duration: const Duration(seconds: 15),
     );
-
-    lottiecontroller2 = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    );
-
-    lottiecontroller3 = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    );
-
-
-    
 
     controller = controller1;
-    lottiecontroller = lottiecontroller1;
 
     controller1.addStatusListener((AnimationStatus status) {
       if (status == AnimationStatus.dismissed) {
@@ -113,42 +97,12 @@ class _CountDownTimerState extends State<CountDownTimer>
       }
     });
 
-
-    lottiecontroller1.addStatusListener((AnimationStatus status) { 
-      if (status == AnimationStatus.dismissed) {
-         setState(() {
-           lottiecontroller = lottiecontroller2;
-         });
-         lottiecontroller2.stop();
-      }
-
-
-
-    });
-    lottiecontroller2.addStatusListener((AnimationStatus status) { 
-      if (status == AnimationStatus.dismissed) {
-         setState(() {
-           lottiecontroller = lottiecontroller3;
-         });
-         lottiecontroller3.reverse(from: 1.0);
-      }
-
-
-
-    });
-    lottiecontroller3.addStatusListener((AnimationStatus status) { 
-      if (status == AnimationStatus.completed) {
-         setState(() {
-           lottiecontroller = lottiecontroller1;
-         });
-         lottiecontroller1.forward(from: 0);
-      }
-
-
-
+    lottiecontroller.addStatusListener((status) {
+      setState(() {
+        lottiecontroller.forward();
+      });
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -171,12 +125,12 @@ class _CountDownTimerState extends State<CountDownTimer>
                             aspectRatio: 1,
                             child: Stack(
                               children: <Widget>[
-                                
                                 Positioned.fill(
                                   child: CustomPaint(
                                     painter: CustomTimerPainter1(
                                       animation: controller1,
-                                      backgroundColor: const Color.fromARGB(255, 151, 240, 128),
+                                      backgroundColor:
+                                          Color.fromARGB(255, 129, 131, 129),
                                     ),
                                   ),
                                 ),
@@ -184,7 +138,8 @@ class _CountDownTimerState extends State<CountDownTimer>
                                   child: CustomPaint(
                                     painter: CustomTimerPainter2(
                                       animation: controller2,
-                                      backgroundColor: const Color.fromARGB(255, 151, 240, 128),
+                                      backgroundColor: const Color.fromARGB(
+                                          255, 129, 131, 129),
                                     ),
                                   ),
                                 ),
@@ -192,52 +147,52 @@ class _CountDownTimerState extends State<CountDownTimer>
                                   child: CustomPaint(
                                     painter: CustomTimerPainter3(
                                       animation: controller3,
-                                      backgroundColor: const Color.fromARGB(255, 151, 240, 128),
+                                      backgroundColor: const Color.fromARGB(
+                                          255, 129, 131, 129),
                                     ),
                                   ),
                                 ),
-
                                 Align(
-                                  alignment: Alignment.bottomCenter,                         
+                                  alignment: Alignment.bottomCenter,
                                   child: Stack(
-                                   
-
-                                    children: <Widget>[  
+                                    children: <Widget>[
                                       Positioned.fill(
-      child: Align(
-        alignment: Alignment.centerRight,
+                                        child: Align(
+                                          alignment: Alignment.centerRight,
 
-        child: Container(height: 400,
-        child: Lottie.asset('assets/breathing.json',controller: lottiecontroller),
-        ),
-        
-                                       
-                                       
-                                        // child: Breather(),
-                                        ),),    
-                                      Positioned(   
-                                        left:150.0,
-                                        bottom:200.0,
-                                        child:                            
-                                      Text(
-                                        timerText,
-                                        style: const TextStyle(
-                                          fontSize: 30.0,
-                                          color: Color(0xFF1D1E33),
+                                          child: Container(
+                                            height: 800,
+                                            child: Lottie.asset(
+                                                'assets/breathing.json',
+                                                controller: lottiecontroller,
+                                                repeat: true),
+                                          ),
+
+                                          // child: Breather(),
                                         ),
-                                      ),),
-                                      Positioned(   
-                                        left:160.0,
-                                        bottom:70.0,
-                                        child: 
-                                      Text(
-                                        timerString,
-                                        style: const TextStyle(
-                                          fontSize: 112.0,
-                                          color: Color(0xFF1D1E33),
+                                      ),
+                                      Positioned(
+                                        left: 150.0,
+                                        bottom: 200.0,
+                                        child: Text(
+                                          timerText,
+                                          style: const TextStyle(
+                                            fontSize: 30.0,
+                                            color: Color(0xFF1D1E33),
+                                          ),
                                         ),
-                                      ),),
-   
+                                      ),
+                                      Positioned(
+                                        left: 160.0,
+                                        bottom: 70.0,
+                                        child: Text(
+                                          timerString,
+                                          style: const TextStyle(
+                                            fontSize: 112.0,
+                                            color: Color(0xFF1D1E33),
+                                          ),
+                                        ),
+                                      ),
                                     ],
                                   ),
                                 ),
@@ -253,23 +208,25 @@ class _CountDownTimerState extends State<CountDownTimer>
                                 onPressed: () {
                                   if (controller.isAnimating)
                                     controller.stop();
-                                    
                                   else {
                                     controller.reverse(
                                         from: controller.value == 0.0
                                             ? 1.0
                                             : controller.value);
-                                  }  
-                                  if (lottiecontroller.isAnimating)
-                                    lottiecontroller.stop();
-                                    
-                                     
-                                  else {
-                                    lottiecontroller.forward(from:lottiecontroller.value);
-                                  }   
+                                  }
 
-                                  final player = AudioCache(); 
-                                  //player.play('');  add audio file to assets                   
+                                  if (lottiecontroller.isAnimating) {
+                                    lottiecontroller.stop();
+                                  } else {
+                                    lottiecontroller.forward(
+                                        from: lottiecontroller.value);
+                                  }
+
+                                  if (lottiecontroller.isCompleted) {
+                                    lottiecontroller.repeat();
+                                  }
+
+                                  final player = AudioCache();
                                 },
                                 icon: Icon(controller.isAnimating
                                     ? Icons.pause
@@ -277,9 +234,7 @@ class _CountDownTimerState extends State<CountDownTimer>
                                 label: Text(
                                     controller.isAnimating ? "Pause" : "Play"));
                           }),
-                          
                     ],
-                    
                   ),
                 ),
               ],
